71f9995e356f3ad07d41a77d565574ecb2ea9fb9
Douglas Suárez Zamorano <d.suarez.zamorano@gmail.com>
2025-06-30 20:42:02 -0400

    2025-06-30 20:36 / Se agrega hipervínculo y favicon.ico

--- js/scripts.js
+++ js/scripts.js
@@ -8,31 +8,55 @@ function showAlert(user_id) {
     let users = {
         1: {
             title: "Valeria Jara",
-            html: `<p>Lorem Ipsum</p><p>Datos de contacto:<br>Teléfono: <a href='tel:+56912345678'>+56 9 1234 5678</a><br>Correo: <a href='#'>jane@doe.com</a><br>LinkedIn: <a href='https://www.linkedin.com/in/valeria-jara-bugue%C3%B1o-364591268/' target='_blank'>Valeria Jara Bugueño</a></p>`
+            html: `<p>Lorem Ipsum - <span class="fst-italic">Lorem Ipsum</span></p>
+            <p class="fw-medium mb-1 text-decoration-underline">Datos de contacto:</p>
+            <p class="mb-0">Teléfono: <a href='tel:+56912345678'>+56 9 1234 5678</a></p>
+            <p class="mb-0">Correo: <a href='#'>jane@doe.com</a></p>
+            <p class="mb-0">LinkedIn: <a href='https://www.linkedin.com/in/valeria-jara-bugue%C3%B1o-364591268/' target='_blank'>Valeria Jara Bugueño</a></p>`
         },
         2: {
             title: "Cristian Astudillo",
-            html: `<p>Lorem Ipsum</p><p>Datos de contacto:<br>Teléfono: <a href='tel:+56987654321'>+56 9 8765 4321</a><br>Correo: <a href='#'>john@doe.com</a><br>LinkedIn: <a href='https://www.linkedin.com/in/crastudillo/' target='_blank'>Cristian Astudillo B.</a></p>`
+            html: `<p>Lorem Ipsum - <span class="fst-italic">Lorem Ipsum</span></p>
+            <p class="fw-medium mb-1 text-decoration-underline">Datos de contacto:</p>
+            <p class="mb-0">Teléfono: <a href='tel:+56987654321'>+56 9 8765 4321</a></p>
+            <p class="mb-0">Correo: <a href='#'>john@doe.com</a></p>
+            <p class="mb-0">LinkedIn: <a href='https://www.linkedin.com/in/crastudillo/' target='_blank'>Cristian Astudillo B.</a></p>`
         },
         3: {
             title: "Gerard Bourguett",
-            html: `<p>Lorem Ipsum</p><p>Datos de contacto:<br>Teléfono: <a href='tel:+56911223344'>+56 9 1122 3344</a><br>Correo: <a href='#'>john@doe.com</a><br>LinkedIn: <a href='https://www.linkedin.com/in/gerard-bourguett/' target='_blank'>Gerard Bourguett</a></p>`
+            html: `<p>Lorem Ipsum - <span class="fst-italic">Lorem Ipsum</span></p>
+            <p class="fw-medium mb-1 text-decoration-underline">Datos de contacto:</p>
+            <p class="mb-0">Teléfono: <a href='tel:+56911223344'>+56 9 1122 3344</a></p>
+            <p class="mb-0">Correo: <a href='#'>john@doe.com</a></p>
+            <p class="mb-0">LinkedIn: <a href='https://www.linkedin.com/in/gerard-bourguett/' target='_blank'>Gerard Bourguett</a></p>`
         },
         4: {
             title: "Emilio Villegas",
-            html: `<p>Jefe de Procesamiento de Datos - Cadem</p><p>Datos de contacto:<br>Teléfono: <a href='tel:+56948519911'>+56 9 4851 9911</a><br>Correo: <a href='mailto:emilio.villegas@cadem.cl'>emilio.villegas@cadem.cl</a><br>LinkedIn: <a href='https://www.linkedin.com/in/emilio-villegas-755251a5/' target='_blank'>Emilio Villegas</a></p>`
+            html: `<p>Jefe de Procesamiento de Datos - <span class="fst-italic">Cadem</span></p>
+            <p class="fw-medium mb-1 text-decoration-underline">Datos de contacto:</p>
+            <p class="mb-0">Teléfono: <a href='tel:+56948519911'>+56 9 4851 9911</a></p>
+            <p class="mb-0">Correo: <a href='mailto:emilio.villegas@cadem.cl'>emilio.villegas@cadem.cl</a></p>
+            <p class="mb-0">LinkedIn: <a href='https://www.linkedin.com/in/emilio-villegas-755251a5/' target='_blank'>Emilio Villegas</a></p>`
         },
         5: {
             title: "Sergio Poblete",
-            html: `<p>Jefe de Personal - Facultad de Ciencias Veterinarias y Pecuarias Universidad de Chile</p><p>Datos de contacto:<br>Teléfono: <a href='tel:+56979839392'>+56 9 7983 9392</a></p>`
+            html: `<p>Jefe de Personal - <span class="fst-italic">Facultad de Ciencias Veterinarias y Pecuarias Universidad de Chile</span></p>
+            <p class="fw-medium mb-1 text-decoration-underline">Datos de contacto:</p>
+            <p class="mb-0">Teléfono: <a href='tel:+56979839392'>+56 9 7983 9392</a></p>`
         },
         6: {
             title: "Luis Vega",
-            html: `<p>Supervisor de URP - Banco de Chile</p><p>Datos de contacto:<br>Teléfono: <a href='tel:+56930221394'>+56 9 3022 1394</a><br>Correo: <a href='mailto:levega@bancochile.cl'>levega@bancochile.cl</a></p>`
+            html: `<p>Supervisor de URP - <span class="fst-italic">Banco de Chile</span></p>
+            <p class="fw-medium mb-1 text-decoration-underline">Datos de contacto:</p>
+            <p class="mb-0">Teléfono: <a href='tel:+56930221394'>+56 9 3022 1394</a></p>
+            <p class="mb-0">Correo: <a href='mailto:levega@bancochile.cl'>levega@bancochile.cl</a></p>`
         },
         7: {
             title: "Joaquín Gonzalez",
-            html: `<p>Programador Web de Encuestas - Cadem</p><p>Datos de contacto:<br>Teléfono: <a href='tel:+56930221394'>+56 9 3022 1394</a><br>Correo: <a href='mailto:joaquin.gonzalez@cadem.cl'>joaquin.gonzalez@cadem.cl</a></p>`
+            html: `<p>Programador Web de Encuestas - <span class="fst-italic">Cadem</span></p>
+            <p class="fw-medium mb-1 text-decoration-underline">Datos de contacto:</p>
+            <p class="mb-0">Teléfono: <a href='tel:+56930221394'>+56 9 3022 1394</a></p>
+            <p class="mb-0">Correo: <a href='mailto:joaquin.gonzalez@cadem.cl'>joaquin.gonzalez@cadem.cl</a></p>`
         }
     };
     const user = users[user_id];
